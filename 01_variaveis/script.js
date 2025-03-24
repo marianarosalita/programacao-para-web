3b414799c24ba35ccee4c731a1f6954e467378c4
Mariana <162517839+marianarosalita@users.noreply.github.com>
2025-03-23 22:03:20 -0300

    Add files via upload

---
+++ 01_variaveis/script.js
@@ -0,0 +1,57 @@
+
+var nome="Francisco";
+
+console.log(nome);
+
+let sobrenome="Santos";
+
+const ensinoSuperior = true;
+
+var preco = 20;
+var totalCompras = 5;
+var precoTotal = preco * totalCompras;
+
+console.log(precoTotal);
+
+var nome="Antonio", idade=29, faculdade = true
+
+console.log(nome, idade, faculdade);
+
+var semValor;
+
+console.log(semValor);
+
+// definir os nomes das variáveis.
+// $, _, letras;
+// case sensitive
+// nome != Nome
+// Camel case;
+// nomeDoAluno;
+// valorDoPagamento;
+// nome simples
+// nome
+// nome Composto.
+// nomeDoAlgumaCoisa
+
+
+//Declare uma variável com seu nome
+
+var nome = "Mariana"
+console.log(nome)
+
+//Declare uma variável com a sua idade
+
+var idade = 25;
+console.log(idade)
+
+//Declare uma variável com a sua comida favorita
+
+var comida = "Pizza";
+console.log(comida)
+
+//Declare 5 variáveis com diferentes
+
+var x=10, y=20, z=5, a="valor", b="total";
+
+console.log(x,y,z,a,b);
+
